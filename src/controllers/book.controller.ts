c822ccaf816df517b8447acada74e0f9becb6056
Jair Barcasnegras <jbarcasnegras@juridigo.com>
2023-05-06 23:18:46 -0500

    feat: book classification trend by rank and readinglist

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -2,6 +2,7 @@ import express, { Router } from "express";
 import { createBook, deleteBook, getAllBooks, getBookById } from "../use-cases/book.use-case";
 import { getUserById } from "../use-cases/user.use-case";
 import { createRank, getRankByUserAndBok, updateRank } from "../use-cases/rank.user-case";
+import { orderBookByTrend } from "../utils/book.utils";
 
 const router: Router = express.Router();
 
@@ -22,6 +23,16 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
     const bookList = await getAllBooks();
     return res.status(200).json({
+        status: "success",
+        data: bookList
+    });
+});
+
+
+router.get("/trend", async (req, res) => {
+    let bookList = await getAllBooks();
+    bookList = orderBookByTrend(bookList);
+    return res.status(201).json({
         status: "success",
         data: bookList
     });
@@ -58,7 +69,7 @@ router.post("/rank", async (req, res) => {
     if (book == undefined) return res.status(404).json({ status: "error", message: "book not found" });
 
     const foundRank = await getRankByUserAndBok(user.id, book.id);
-    if (foundRank !== undefined) {        
+    if (foundRank !== undefined) {
         await updateRank(foundRank.id, { score: parseInt(`${score}`) });
     } else {
         user = await createRank(parseInt(`${score}`), user, book);
@@ -71,5 +82,8 @@ router.post("/rank", async (req, res) => {
         data: book
     });
 });
+
+
+
 
 export const BookController = router;
